40ecd4a7136484cb4207ac28736f78ea55fd0c49
Haosen <haosenli2001@gmail.com>
2022-11-29 03:09:38 -0800

    update

--- anime-search-web-app/pages/_app.tsx
+++ anime-search-web-app/pages/_app.tsx
@@ -14,5 +14,5 @@ export default function App({ Component, pageProps }: AppProps) {
         <Component {...pageProps} />
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossOrigin="anonymous"></script>
     </Layout>
-  )
-}
+  );
+};
