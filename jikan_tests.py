d3c51bd1f9c0de32358aa9ba68480a984b868e36
Haosen <haosenli2001@gmail.com>
2023-01-02 15:47:02 -0800

    updated test

--- jikan_tests.py
+++ jikan_tests.py
@@ -1,5 +1,31 @@
 import requests
 
+def get_top_anime():
+    url = 'https://api.jikan.moe/v4/top/anime?filter=airing'
+    response = requests.get(url)
+    if response.status_code != 200:
+        print('[ERROR] status code:', response.status_code)
+        return
+    data = response.json()
+    for char in data['data']:
+        for k, v in char.items():
+            if k == 'title' or k == 'status'or k == 'images':
+                print(k, v)
+        print()
+
+def get_season_now():
+    url = 'https://api.jikan.moe/v4/seasons/now'
+    response = requests.get(url)
+    if response.status_code != 200:
+        print('[ERROR] status code:', response.status_code)
+        return
+    data = response.json()
+    for char in data['data']:
+        for k, v in char.items():
+            if k == 'title' or k == 'status'or k == 'broadcast':
+                print(k, v)
+        print()
+
 def get_anime_full():
     anime_id = 34798
     url = f'https://api.jikan.moe/v4/anime/{anime_id}/full'
@@ -69,7 +95,20 @@ def get_person_full_by_id():
         print(holy, sht)
 
 def get_schedule():
-    url = 'https://api.jikan.moe/v4/schedules'
+    url = 'https://api.jikan.moe/v4/schedules/?filter=unknown'
+    response = requests.get(url)
+    if response.status_code != 200:
+        print('[ERROR] status code:', response.status_code)
+        return
+    data = response.json()
+    for char in data['data']:
+        for k, v in char.items():
+            if k == 'title' or k == 'status'or k == 'broadcast':
+                print(k, v)
+        print()
+        
+def get_characters_search():
+    url = 'https://api.jikan.moe/v4/characters/?q=kanna&order_by=favorites&sort=desc'
     response = requests.get(url)
     if response.status_code != 200:
         print('[ERROR] status code:', response.status_code)
@@ -81,12 +120,14 @@ def get_schedule():
         print()
 
 def main():
+    get_top_anime()
     # get_anime_full()
     # get_anime_episodes()
     # get_anime_images()
     # get_anime_videos()
     # get_anime_characters()  
-    get_person_full_by_id()
+    # get_person_full_by_id()
+    # get_characters_search()
     # get_schedule()
 
 if __name__ == '__main__':
